d450d7ce8b0eb9eb074774024febb5c3ab45518a
Mussi <ymussi@gmail.com>
2020-07-25 20:08:02 -0300

    removendo arquivos nao usados e inserindo swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const options = new DocumentBuilder()
+    .setTitle('Crud API')
+    .setDescription('Learn Node and NestJS while doing')
+    .setVersion('1.0')
+    // .addTag('crud')
+    .build()
+  
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
